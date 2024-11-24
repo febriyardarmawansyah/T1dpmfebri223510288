badd71822b8338f3fe76d2c551841ceba86ba430
febri <febriyar@gmail.com>
2024-11-24 19:23:15 +0700

    Initial commit for P2-LAB-DPM project

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Haii Febri Yar Darmawansyah P </Text>
+      <View style={styles.boxContainer}>
+        <View style={styles.box1} />
+        <View style={styles.box2} />
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#D3D3D3', // Abu-abu terang
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    color: 'blue',
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  boxContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%',
+  },
+  box1: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red', // Bebas
+  },
+  box2: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'green', // Bebas
   },
 });
+
+export default App;
